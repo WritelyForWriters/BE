b071850bc8da8f18b5e529cc1116239906139091
SBSun <byung0216@gmail.com>
2025-01-17 22:56:05 +0900

    [feat] init

---
+++ src/main/java/com/writely/WritelyApplication.java
@@ -0,0 +1,13 @@
+package com.writely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WritelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WritelyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/writely/WritelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.writely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WritelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
