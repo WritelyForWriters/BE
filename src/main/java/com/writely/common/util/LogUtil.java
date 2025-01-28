6fc31d180c358de1c97d2a6be91d82381fc5b8a3
Wichan Kang <wichan7@naver.com>
2025-01-29 02:22:15 +0900

    refactor(log): stacktrace를 파라미터로 하는 오버로딩 함수 추가 (#17)
    Co-authored-by: SBSun <byung0216@gmail.com>

--- src/main/java/com/writely/common/util/LogUtil.java
+++ src/main/java/com/writely/common/util/LogUtil.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 @Slf4j
 @UtilityClass
 public class LogUtil {
@@ -14,20 +17,46 @@ public class LogUtil {
   public void debug(String msg) {
     LOG.info(msg);
   }
+  public void debug(Throwable throwable) {
+    LOG.info(getStackTrace(throwable));
+  }
 
   public void trace(String msg) {
     LOG.trace(msg);
+  }
+  public void trace(Throwable throwable) {
+    LOG.trace(getStackTrace(throwable));
   }
 
   public void info(String msg) {
     LOG.info(msg);
   }
+  public void info(Throwable throwable) {
+    LOG.info(getStackTrace(throwable));
+  }
 
   public void warn(String msg) {
     LOG.warn(msg);
+  }
+  public void warn(Throwable throwable) {
+    LOG.warn(getStackTrace(throwable));
   }
 
   public void error(String msg) {
     LOG.error(msg);
   }
+  public void error(Throwable throwable) {
+    LOG.error(getStackTrace(throwable));
+  }
+
+  private String getStackTrace(Throwable throwable) {
+    if (throwable == null) {
+      return "No exception provided.";
+    }
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter printWriter = new PrintWriter(stringWriter);
+    throwable.printStackTrace(printWriter);
+    return stringWriter.toString();
+  }
+
 }
