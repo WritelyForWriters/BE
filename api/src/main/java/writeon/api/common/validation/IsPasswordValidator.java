748cf64c1ade3b67bec900561ea8fc8f3594b694
wichan <wichan7@naver.com>
2025-06-12 21:47:17 +0900

    fix: 비밀번호 정규식 변경

--- api/src/main/java/writeon/api/common/validation/IsPasswordValidator.java
+++ api/src/main/java/writeon/api/common/validation/IsPasswordValidator.java
@@ -3,28 +3,16 @@ package writeon.api.common.validation;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-import java.util.stream.Stream;
-
 public class IsPasswordValidator implements ConstraintValidator<IsPassword, String> {
 
-    final String digit = "0-9";
-    final String uppercase = "A-Z";
-    final String lowercase = "a-z";
-    final String specialCharacters = "@$!%*?&";
+    final String regexp
+            = "(?=(.*[A-Z]))(?=(.*[a-z]))(?=(.*\\d))(?=(.*[\\W_])).{8,}$|^(?=(.*[A-Z]))(?=(.*[a-z]))(?=(.*\\d)).{8,}$|^(?=(.*[A-Z]))(?=(.*[a-z]))(?=(.*[\\W_])).{8,}$|^(?=(.*[A-Z]))(?=(.*\\d))(?=(.*[\\W_])).{8,}$|^(?=(.*[a-z]))(?=(.*\\d))(?=(.*[\\W_])).{8,}";
 
     @Override
     public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
         if (s == null)
             return true;
 
-        final String otherCharacterMatcher = String.format(".*[^%s%s%s%s].*", digit, uppercase, lowercase, specialCharacters);
-        if (s.matches(otherCharacterMatcher))
-            return false;
-
-        long matchCount = Stream.of(digit, uppercase, lowercase, specialCharacters)
-                .filter(regex -> s.matches(String.format(".*[%s].*", regex)))
-                .count();
-
-        return matchCount >= 3;
+        return s.matches(regexp);
     }
 }
